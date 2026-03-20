d17960cf00818965e46a91c692e5b7c55a6333f6
Julien Marc <mail@julienmarcbrown.com>
2026-03-19 17:01:06 -0700

    Update sem-lite formula to v0.9.0 with real checksums

--- Formula/sem-lite.rb
+++ Formula/sem-lite.rb
@@ -7,22 +7,22 @@
 class SemLite < Formula
   desc "Local-first documentation intelligence CLI"
   homepage "https://github.com/semwerk/sem-lite"
-  version "0.8.0"
+  version "0.9.0"
   license "BSL-1.1"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/semwerk/sem-lite/releases/download/v#{version}/sem-lite_#{version}_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
+      sha256 "7fdcfbb8ca6c2ad018ba3595a1101f07cd481f2ebbda1b6c20116c657908d207"
     else
       url "https://github.com/semwerk/sem-lite/releases/download/v#{version}/sem-lite_#{version}_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
+      sha256 "44e8377ac95bf1b7f8bb076cb94dd4ee9dd4555b2276c8d82eb7e31c3bde666b"
     end
   end
 
   on_linux do
     url "https://github.com/semwerk/sem-lite/releases/download/v#{version}/sem-lite_#{version}_linux_amd64.tar.gz"
-    sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+    sha256 "e329a1c60f3d7a095c560b8d36df345a0ab355f28ead1e14c0c8e1f401cbf3b5"
   end
 
   def install
